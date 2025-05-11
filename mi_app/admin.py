79e9e05d4959cf2100a4b8cc80abce83c34a7051
Xhector <raulromerog002@gmail.com>
2025-05-11 15:49:38 -0500

    Segundo Commit

--- mi_app/admin.py
+++ mi_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Cargo, Departamento, Empleado, Rol, TipoContrato
 
 # Register your models here.
+admin.site.register(Cargo)
+admin.site.register(Departamento)
+admin.site.register(Empleado)
+admin.site.register(Rol)
+admin.site.register(TipoContrato)
