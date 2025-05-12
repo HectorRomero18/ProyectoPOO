b546af9880ebdc18be7ad1c49d58667f10de214f
Xhector <raulromerog002@gmail.com>
2025-05-11 19:33:53 -0500

    Haciendo el crud para el modelo Cargo

--- mi_app/urls.py
+++ mi_app/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from mi_app.views import  home
+from .import  views
 app_name = 'core'  # Nombre de la aplicación para el espacio de nombres
 urlpatterns = [
-    path('', home, name='home'),
+    path('', views.home, name='home'),
+    
+    # *****************URL PARA EL MODELO CARGO**********************
+    path('create_cargo', views.create_cargo, name='create_cargo'),
+    path('list_cargo', views.mostrar_cargos, name='list_cargo'),
+    path('delete_cargo/<int:id>', views.delete_cargo, name='delete_cargo'),
+    path('update_cargo/<int:id>', views.update_cargo, name='update_cargo')
+
+    # *********************                       **********************
 
 ]
 
--- mi_app/views.py
+++ mi_app/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect, get_object_or_404
+from .models import Cargo
+from .forms import CargoForm
 
 
 def home(request):
     data = {
-        'title': 'App Medica',
+        'title': 'APP DE NO SE QUE',
         'description': 'Gesion de citas medicas',
         'author': 'Daniel Vera',
         'year': 2025,
@@ -14,4 +16,41 @@ def home(request):
     #return HttpResponse("<h1>Hello,Mi primer pagina con django</h1>")
     #return JsonResponse(data)  
     return render(request, 'home.html', data)
+
+
+
+
+# Vistas para el CRUD del modelo Cargo
+def create_cargo(request):
+    
+    if request.method == 'POST':
+        form = CargoForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+            return redirect('core:list_cargo')
+    else:
+        form = CargoForm()
+    return render(request, 'Cargo/create_cargo.html', {'form': form})
+
+def mostrar_cargos(request):
+    cargos = Cargo.objects.all()
+    return render(request, 'Cargo/list.html', {'cargos': cargos})
+
+def delete_cargo(request, id):
+    cargo = get_object_or_404(Cargo, id=id)
+    cargo.delete()
+    return redirect('core:list_cargo')
+
+def update_cargo(request, id):
+    cargo = get_object_or_404(Cargo, id=id)
+    if request.method == 'POST':
+        form = CargoForm(request.POST, instance=cargo)
+        if form.is_valid():
+            form.save()
+            return redirect('core:list_cargo')
+    else:
+        form = CargoForm(instance=cargo)
+    return render(request, 'Cargo/update_cargo.html', {'form': form})
+
+
 
